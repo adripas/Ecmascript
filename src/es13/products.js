3f9946a8deaae633ad02bf32c9dfb7681ff4749f
adripas <adripas@gmail.com>
2023-01-10 14:43:53 -0500

    commit3

--- src/es13/products.js
+++ src/es13/products.js
@@ -1,8 +1,10 @@
+// Se instala libreria npm install node-fetch par apoder usar fetch
 import fetch from "node-fetch";
-const response = await fetch('htpps://api.escuelajs.co/api/v1/products'); // se genera constante peticion para obtener datos.
-const products = await response.json();   //resultante dle llamado, lo qeu recibimos lo transformamos en un objeto json
-export {products}; // se exportan lso productos para usarlo en otro documento, volviendolo un modulo para usarse donde sea necesario
+const response =  fetch('https://api.escuelajs.co/api/v1/products'); // se genera constante peticion para obtener datos.
+const products = response.json();   //resultante del llamado, lo que recibimos lo transformamos en un objeto json
 
-// ya no es necesario usar la palabra async, antes se tenia que encapsular una funcion que s epudiera llamar get prouct hacer logica y retorna product y exportar la funcion para poderla llamar y poder usar asyn y await 
+export { products }; // se exportan los productos para usarlo en otro documento, volviendolo un modulo para usarse donde sea necesario
+// ya no es necesario usar la palabra async, antes se tenia que encapsular una funcion que se pudiera llamar get product hacer logica y retorna product y exportar la funcion para poderla llamarla donde sea necesario  y poder usar asyn y await
 
-
+//cuando llega esa logia o puede procesar la información, por esto await nos  ayuda a que espera a que regrese la repsuesta del  servidor y luego transforma a un objetos que podemos usar y mostramelos.
+// y eso del top leve sin el uso directo de async
